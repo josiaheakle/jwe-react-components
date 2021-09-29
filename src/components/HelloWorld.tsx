e1cacd77820989c4799562eec938e5b1fa8841cf
Josiah Eakle <josiaheakle@gmail.com>
2021-09-29 11:46:54 -0400

    init commit

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface HelloWorldProps {}
+
+const HelloWorld: React.FC<HelloWorldProps> = ({}) => {
+	return <article>HELLO WORLD</article>;
+};
+
+export { HelloWorld };
