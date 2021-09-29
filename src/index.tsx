dc4dd3802ddcd8f3ad7fd2e78748d7b252fa7c92
Josiah Eakle <josiaheakle@gmail.com>
2021-09-29 12:05:25 -0400

    init setup for easy development

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HelloWorld } from "./lib";
+
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<HelloWorld />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
--- src/lib/components/HelloWorld.tsx
+++ src/lib/components/HelloWorld.tsx
@@ -3,6 +3,10 @@ import * as React from "react";
 interface HelloWorldProps {}
 
 const HelloWorld: React.FC<HelloWorldProps> = ({}) => {
+	React.useEffect(() => {
+		console.log(`hello world`);
+	}, []);
+
 	return <article>HELLO WORLD</article>;
 };
 
